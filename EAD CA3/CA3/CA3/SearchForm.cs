1f59d891edd76ac41511132cf27dc50f36cf6e91
X00145679 <43677677+X00145679@users.noreply.github.com>
2020-12-09 22:01:59 +0000

    Reviews API call via search working

---
+++ EAD CA3/CA3/CA3/SearchForm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CA3
+{
+    public class SearchForm
+    {
+        [Required]
+        [StringLength(18, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
